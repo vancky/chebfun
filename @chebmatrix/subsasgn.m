d80d19c6d53055b8e93c06c1bc988a4dcf01eab0
Hrothgar <hrothgar@maths.ox.ac.uk>
2015-03-02 18:23:29 +0000

    Allowing element deletion via A(1,1)=[] for CHEBMATRIX.

--- @chebmatrix/subsasgn.m
+++ @chebmatrix/subsasgn.m
@@ -3,24 +3,27 @@ function A = subsasgn(A, sa, B)
 %   A(...) = B replaces the block or submatrix referenced on the left side
 %   of the equality with the blocks given on the right side.
 %
-%   A.(...) = B changes a named property of A. 
+%   A.(...) = B changes a named property of A.
 
 % Copyright 2014 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
 switch(sa(1).type)
-    
+
     case {'()', '{}'}
-        if isa(B, 'chebmatrix')
+        data = [];
+        if ( isa(B, 'chebmatrix') )
             data = B.blocks;
-        else
+        elseif ( ~isempty(B) )
+            % Empty B results in removing the referenced elements:
+            % >> A(1,:) = []
             data = { B };
         end
 
         A.blocks = subsasgn(A.blocks, sa, data);
-    
+
     otherwise  % Dot reference (property).
-        
+
         if ( length(sa) > 1 )
             % Nested ref. This occurs when setting a pref, most notably.
             p = A.(sa(1).subs);
