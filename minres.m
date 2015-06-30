4b9087ce7c0a235de0f85316eb904a1f42367ae7
jared <aurentz@maths.ox.ac.uk>
2015-06-30 21:08:17 +0100

    first draft of minres

---
+++ minres.m
@@ -0,0 +1,59 @@
+function [u,res] = minres(L,f,tol,maxits)
+% Function MINRES
+
+% Check tol
+if isempty(tol)
+  p = cheboppref;
+  tol = p.errTol;
+end
+
+% check maxits
+if isempty(maxits)
+  maxits = 50;
+end
+
+% Initialize solution
+u = 0*f;
+
+% Initialize Lanczos vectors
+[Q,res] = qr(f);
+if res == 0, return, end
+
+% Initialize H 
+H = [];
+
+% Iteration loop
+for ii = 1:maxits
+  
+  % Apply operator
+  v = L(Q(:,end));
+
+  % orthogonalize
+  d1 = Q'*v; v = v - Q*d1;
+
+  % reorthogonalize
+  d2 = Q'*v; v = v - Q*d2;
+
+  % normalize
+  s1 = norm(v); 
+  if s1 == 0
+    warning('CHEBFUN/MINRES: operation terminated before convergence')
+    break
+  end
+
+  % update H
+  H(1:ii+1,ii) = [d1+d2;s1];
+
+  % solve least squares problem
+  u = Q*(H\[res(1);zeros(ii,1)]);
+
+  % compute residual
+  res = [res;norm(f-L(u))];
+  if res(end) < tol*res(1)
+    break
+  end
+
+  % update Q
+  Q = [Q,v/s1];
+
+end
