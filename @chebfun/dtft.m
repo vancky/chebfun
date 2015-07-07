a9206aa7bc3bc9198db29d457f9e4260302e4286
Mohsin Javed <mhsnjvd@gmail.com>
2015-07-08 00:01:22 +0100

    adding dtft and its inverse.

---
+++ @chebfun/dtft.m
@@ -0,0 +1,26 @@
+function X = dtft(x)
+%DTFT   Discrete time Fourier transform of a discrete signal x. 
+%   X = DTFT(x) returns the discrete time Fourier transform of the signal
+%   x[n] as a periodic chebfun on [-pi, pi]. x[n] must be a vector of 
+%   doubles. The definition of DTFT is what is standard in signal 
+%   porocessing:
+%     X(w) = sum_{n=-inf}^{n=inf} ( x[n]*exp(-1i*w*n) )
+%   Notice the minus sign in the epxonential above.
+
+% Copyright 2015 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+if ( isempty(x) )
+    X = chebfun;
+    return
+end
+
+if ( ~isvector(x) || ~isa(x, 'double') )
+    error( 'CHEBFUN:DTFT', 'x must be a vector of doubles')
+end
+
+%%
+% Make sure x is a column vector, and flip it due to the definition of
+% DTFT:
+x = flipud(x(:));
+X = chebfun(x, [-pi, pi], 'coeffs', 'trig');
---
+++ @chebfun/idtft.m
@@ -0,0 +1,31 @@
+function x = idtft(X)
+%DTFT   Inverse Discrete time Fourier transform of a chebfun X. 
+%   x = DTFT(X) returns the inverse discrete time Fourier transform of the 
+%   periodic chehbfun X on [-pi, pi]. The returned x is a vector of
+%   doubles. The definition of IDTFT is what is standard in signal 
+%   porocessing:
+%     x[n] = 1/(2*pi)int_{pi}^{pi} X(w)*exp(1i*w*n) dw
+
+% Copyright 2015 by The University of Oxford and The Chebfun Developers.
+% See http://www.chebfun.org/ for Chebfun information.
+
+if ( isempty(X) )
+    x = [];
+    return
+end
+dom = domain(X);
+a = dom(1);
+b = dom(end);
+
+if ( ~isa(X, 'chebfun') || ~isPeriodicTech(X) )
+    error( 'CHEBFUN:IDTFT', 'X must be a periodic chebfun.')
+end
+
+if ( norm([a, b] - [-pi, pi], inf) > 1e-13 )
+    error( 'CHEBFUN:IDTFT', 'domain of X must be [-pi, pi].')
+end
+
+%%
+% Make sure x is a column vector, and flip it due to the definition of
+% DTFT:
+x = flipud(X.coeffs);
